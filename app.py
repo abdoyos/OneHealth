8586ef327d868be4c15c821180588fbb89926603
abdoyos <43090694+abdoyos@users.noreply.github.com>
2022-10-01 10:34:27 +0300

    updated postgres connection

--- app.py
+++ app.py
@@ -33,11 +33,11 @@ password = "ghostsrv"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return accounts.query.get(int(user_id))
+    return accounts_new.query.get(int(user_id))
 
 ######################################
 
-class accounts(UserMixin, db.Model):
+class accounts_new(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_code = db.Column(db.String(50))
@@ -82,7 +82,7 @@ def login():
     if request.method == "POST":
         passwords = request.form.get('Pass')
         email = request.form.get('email')
-        user = accounts.query.filter_by(user_name=email).first()
+        user = accounts_new.query.filter_by(user_name=email).first()
         next_page = request.args.get("next")
 
         if user:
@@ -112,223 +112,6 @@ def Fogetpass():
 
    return render_template('forgot-password.html')
 
-@app.route('/LabSupport/Monitoring/Timisoara', methods=['GET','POST'])
-@login_required
-def Timis():
-
-
-     cur.execute("select host,comment, case when access = 1 then 'RSH' else 'SSH' end as access,type,case when just_ping =1 then 'Yes' else 'No' end as jus_ping,case when timisoara.ignore =1 then 'Yes' else 'No' end as jus_pingf, case when aut_fail =1 then 'Yes' else 'No' end as aut_fail, shutdown from timisoara;")
-     data = cur.fetchall()
-     if request.method == "POST":
-
-         cur.execute("select host from timisoara;")
-         hosts = cur.fetchall()
-         myssh = Connection(host=hostname, username=username,  password=password)
-         with open("ping_results.txt", "a") as myfile:
-              for x in hosts:
-                z = myssh.execute(command='ping -c 1 %s' %(x))             
-                if "unknown" in z[0]:
-                    myfile.write("%s: the host unknown \n" %(x))
-                    myfile.write("\n")
-                elif "Unreachable" in z[1]:
-                    myfile.write("%s: Unreachable ping is KO & is down!" %(x))
-                    myfile.write("\n")
-                elif "100%" in z[3]:
-                    myfile.write("%s: ping is KO & is down!" %(x))
-                    myfile.write("\n")
-                else:
-                    myfile.write("%s: ping is OK & is up!" %(x) )
-                    myfile.write("\n")
-              
-              flash('Sucess Ping all Machine','success')
-              return redirect(url_for('Timis'))
-
-         #flash('Sucess Ping all Machine','success')
-         #return redirect(url_for('Timis'))
-                   
-            #return redirect(url_for('Timis'))
-         #if stderr:
-          #   flash(stderr.readlines(),'error')
-           #  return redirect(url_for('Timis'))
-         #else:
-          #  flash(stdout.readlines()[0],'success')
-           # return redirect(url_for('Timis'))
-
-     return render_template('Timisoara.html', data=data)
-
-@app.route('/LabSupport/Monitoring/SHALL', methods=['GET','POST'])
-@login_required
-def SHALL():
-
-     if request.method == "POST":
-
-         cur.execute("select host from timisoara;")
-         x = "accsol106"
-         host="accsol106"
-         myssh = Connection(host=hostname, username=username, password=password)
-         with open("SSH.txt", "a") as myfile:
-                Type = myssh.execute(command= " ssh {} -l ghostsrv uname".format(host))
-                z = myssh.execute(command='ping -c 1 %s' %(x))             
-                print(str(z))
-                
-    
-     return redirect(url_for('Timis'))
-              
-
-
-@app.route('/LabSupport/Monitoring/Timisoara/Threshold', methods=['GET','POST'])
-@login_required
-def Timis_Threshold():
-     cur.execute("select  *  from timisoara_threshold;")
-     data = cur.fetchall()
-     return render_template('Timisoara_Threshold.html', data=data)
-
-@app.route('/LabSupport/Monitoring/Timisoara/shutdownWS', methods=['GET','POST'])
-@login_required
-def SHWS():
-
-
-     if request.method == "POST":
-         passwd = request.form.get('pass')
-         host = request.form.get('shs')
-         rcode = request.form.get('rcode')
-         if passwd == "1":
-               Type = os.system("ssh {} -l ghostsrv uname".format(host))
-               try:
-                  child = pexpect.popen_spawn.PopenSpawn("ssh {} -l ghostsrv uname".format(host))
-                  a = child.expect(['password:', 'The authenticity of host'], timeout=300, async=True)
-                  if a == 0:
-                     child.sendline('123456789')
-                     print('this ip has exists in know_host files!')
-                  if a == 1:
-                     print('this ip will be added to know_host files!')
-                     child.sendline('yes')
-                     child.expect('password:')
-                     child.sendline('123456789')
-                     # send test
-                     child.sendline("df -h")
-                     child.interact()
-               except pexpect.EOF:
-                    traceback.print_exc()
-               if "key" in Type[0]:
-                  child = popen_spawn.PopenSpawn(Type) #'ssh {} -l ghostsrv uname'.format(host))
-                  child.wait()
-                  #child.expect('password:')
-                  #child.sendline(password)
-                  #child.expect(pexpect.EOF)
-                  flash(child.before,'success')
-                  return redirect(url_for('Timis'))
-               else:    
-                 version = myssh.execute(command= "ssh {} -l ghostsrv uname -v".format(host))
-                 release = myssh.execute(command= "ssh {} -l ghostsrv uname -r".format(host))
-               #if Type[0] == "AIX":
-               #    myssh.execute(command= " ssh {} -l ghostsrv /usr/sbin/shutdown -F -h now 2>&1".format(host))
-               #elif Type[0] == "HP-UX":
-               #    myssh.execute(command= " ssh {} -l ghostsrv /usr/sbin/shutdown -y -h now 2>&1".format(host))
-               #elif Type[0] == "SunOS":
-               #    myssh.execute(command= " ssh {} -l ghostsrv /usr/sbin/shutdown -y -i5 -g0 2>&1".format(host))
-               #elif Type[0] == "Linux":
-               #    myssh.execute(command= " ssh {} -l ghostsrv /usr/sbin/shutdown -h now 2>&1".format(host))
-               #else:
-               #    flash('Ignoring: OS unknown for $machine!! ','error')
-               flash('Host:'+ host +' '+'machine:'+ Type[0] +' '+ 'Version:' + version[0] +' '+ 'Release:' + release[0],'success')
-               return redirect(url_for('Timis'))
-            
-     
-          #except:
-           #   flash('Connection failed to Host: %s, please try again' %host,'error')
-            #  return redirect(url_for('Timis'))
-         else:
-             flash('Wrong password, please try again %s' %host,'error')
-             return redirect(url_for('Timis'))
-
-@app.route('/LabSupport/Monitoring/Lannion/Threshold', methods=['GET','POST'])
-@login_required
-def Lan_Threshold():
-     cur.execute("select  *  from timisoara_threshold;")
-     data = cur.fetchall()
-     return render_template('Lan_Threshold.html', data=[])
-
-@app.route('/LabSupport/Monitoring/Stuttgart/Threshold', methods=['GET','POST'])
-@login_required
-def Stutt_Threshold():
-     cur.execute("select  *  from timisoara_threshold;")
-     data = cur.fetchall()
-     return render_template('Stutt_Threshold.html', data=[])
-
-@app.route('/LabSupport/Monitoring/Lannion')
-@login_required
-def Lan():
-    return render_template('Lannion.html')
-
-@app.route('/LabSupport/Monitoring/Stuttgart')
-@login_required
-def Stutt():
-    return render_template('Stuttgart.html')
-
-@app.route('/LabSupport/Monitoring/Daily')
-@login_required
-def daily():
-    return render_template('index.html')
-
-@app.route('/LabSupport/knowledgebase/Doc')
-@login_required
-def Knowledgebase():
-    return render_template('Knowledgebase.html')
-
-
-
-@app.route('/LabSupport/Monitoring/Opale/tickets')
-@login_required
-def OPALE_ticket_how():
-    return render_template('opale_tickets.html')
-
-@app.route('/LabSupport/Monitoring/HCL/Tickets', methods=['GET','POST'])
-@login_required
-def OPALE():
-    
-    cur.execute("select  *  from OPALE;")
-    data = cur.fetchall()
-    Title="HCL Tickets"
-    if request.method == "POST":
-       HCL = request.form.get('HCL')
-       Cdate = request.form.get('Cdate')
-       Status = request.form.get('Status')
-       Escalated = request.form.get('Escalated')
-       #Rdate = request.form.get('Rdate')
-       update = "Insert into OPALE(Ticket_ID,creation_date,Status,Escelated) values(%s, %s, %s, %s)"
-       parameters= (HCL,Cdate,Status,Escalated)
-       cur.execute(update,parameters)
-       conn.commit()
-       flash('The Ticket have been added successfully','success')
-       return redirect(url_for('OPALE'))
-
-    return render_template('OPALE.html', data=data, Title=Title)
-
-
-
-@app.route('/LabSupport/Monitoring/Backup/Details', methods=['GET','POST'])
-@login_required
-def Backup_d():
-    
-    #cur.execute("select  *  from backups_details;")
-    #data = cur.fetchall()
-    Title="Backup Details"
-    if request.method == "POST":
-       docid = request.form.get('Docname')
-       From = request.form.get('From')
-       To = request.form.get('To')
-       Room = request.form.get('Room')
-       #Rdate = request.form.get('Rdate')
-       #update = "Insert into backups_details(hostname,IP,Description,OS,Virtualize,VM_Hosts,Backup_Hosts,Backup_Location_1,Backup_location_2) values(%s, %s, %s, %s,%s, %s, %s, %s,%s)"
-       #parameters= (hostname,ipad,desc,OS,virt,vmhost,bkphost,bkploc1,bkploc2)
-       #cur.execute(update,parameters)
-       #conn.commit()
-       flash('New  record have been added successfully','success')
-       return redirect(url_for('Backup_d'))
-
-    return render_template('backups_details.html', Title=Title)#, data=data, Title=Title)
-
 
 
 @app.route('/onheleath/Pediatrics/schedule/creationO1', methods=['GET','POST'])
@@ -349,7 +132,7 @@ def schedule_create_o1():
        s_from_time = s_from.time()
        s_from_date = s_from.date()
        clinic_id = 1
-       update = 'Insert into schedule.schedule(clinic_id,doc_id,"Date","Day",start_time,end_time,room,comments) values(%s, %s, %s, %s,%s, %s, %s, %s)'
+       update = 'Insert into public.schedule_new(clinic_id,doc_id,"Date","Day",start_time,end_time,room,comments) values(%s, %s, %s, %s,%s, %s, %s, %s)'
        parameters= (clinic_id,int(docid),s_from_date,s_from_day,s_from_time,s_to_time,Room,comm)
        cur.execute(update,parameters)
        conn.commit()
@@ -361,11 +144,11 @@ def schedule_create_o1():
 @login_required
 def O1():
 
-      cur.execute("SELECT id,name FROM schedule.accounts where clinic_id=1 ;")
+      cur.execute("SELECT id,name FROM public.accounts_new where clinic_id=1 ;")
       data1 = cur.fetchall()
-      cur.execute("SELECT user_code FROM schedule.accounts where clinic_id=1 ;")
+      cur.execute("SELECT user_code FROM public.accounts_new where clinic_id=1 ;")
       data2= cur.fetchall()
-      cur.execute('SELECT b.name, b.user_code, b."Speciality", a."Date", a."Day", a.start_time, a.end_time, a.room from schedule.schedule a inner join schedule.accounts b on a.doc_id = b.id where b.clinic_id=1')
+      cur.execute('SELECT b.name, b.user_code, b."Speciality", a."Date", a."Day", a.start_time, a.end_time, a.room from public.schedule_new a inner join public.accounts_new b on a.doc_id = b.id where b.clinic_id=1')
       schedule = cur.fetchall()
       Title="Schedule"
       if request.method == "POST":
